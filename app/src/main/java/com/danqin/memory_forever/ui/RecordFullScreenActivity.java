fca95bbccceb341e845e4ed67e32173518fa44da
jiangliang <jl_qiqi@163.com>
2019-03-23 20:42:35 +0800

    使用VideoView实现全屏播放

--- app/src/main/java/com/danqin/memory_forever/ui/RecordFullScreenActivity.java
+++ app/src/main/java/com/danqin/memory_forever/ui/RecordFullScreenActivity.java
@@ -4,8 +4,6 @@ import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.media.MediaPlayer;
 import android.text.TextUtils;
-import android.util.Log;
-import android.view.SurfaceHolder;
 import android.view.View;
 
 import com.danqin.memory_forever.R;
@@ -14,7 +12,6 @@ import com.danqin.memory_forever.databinding.RecordFullScreenLayoutBinding;
 import com.danqin.memory_forever.view.preview.PreviewItemFragment;
 import com.danqin.memory_forever.view.preview.PreviewPagerAdapter;
 
-import java.io.IOException;
 import java.util.List;
 
 public class RecordFullScreenActivity extends BaseActivity {
@@ -40,27 +37,7 @@ public class RecordFullScreenActivity extends BaseActivity {
             }
         });
         binding.viewpager.setAdapter(pagerAdapter);
-        binding.fullScreenPlay.getHolder().addCallback(new SurfaceHolder.Callback() {
-            @Override
-            public void surfaceCreated(SurfaceHolder holder) {
-                player.setDisplay(holder);
-            }
-
-            @Override
-            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-
-            }
-
-            @Override
-            public void surfaceDestroyed(SurfaceHolder holder) {
-                if (player != null && player.isPlaying()) {
-                    player.stop();
-                }
-            }
-        });
     }
-
-    private MediaPlayer player;
 
     @Override
     protected void loadData() {
@@ -68,14 +45,11 @@ public class RecordFullScreenActivity extends BaseActivity {
         Intent intent = getIntent();
         Record record = (Record) intent.getSerializableExtra(KEY_RECORD);
         String videoUrl = record.getVideoUrl();
-        Log.e(tag, "video url is : " + videoUrl);
         if (!TextUtils.isEmpty(videoUrl)) {
             binding.viewpager.setVisibility(View.GONE);
-            binding.fullScreenPlay.setVisibility(View.VISIBLE);
             playVideo(videoUrl);
         }
         List<String> imgUrls = record.getImgUrls();
-        Log.e(tag, "img urls is : " + imgUrls);
         if (imgUrls != null) {
             binding.fullScreenPlay.setVisibility(View.GONE);
             pagerAdapter.addAllImgs(imgUrls);
@@ -85,40 +59,33 @@ public class RecordFullScreenActivity extends BaseActivity {
     }
 
     private void playVideo(String videoUrl) {
-        try {
-            player = new MediaPlayer();
-            player.setDataSource(videoUrl);
-            player.prepare();
-            player.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
-                @Override
-                public void onPrepared(MediaPlayer mp) {
-                    try {
-                        Thread.sleep(1000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                    mp.start();
-                }
-            });
-            player.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-                @Override
-                public void onCompletion(MediaPlayer mp) {
-                    player.start();
-                }
-            });
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        binding.fullScreenPlay.setVideoPath(videoUrl);
+        binding.fullScreenPlay.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mediaPlayer) {
+                mediaPlayer.start();
+            }
+        });
+        binding.fullScreenPlay.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mediaPlayer) {
+                mediaPlayer.start();
+            }
+        });
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        binding.fullScreenPlay.pause();
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        if (player != null) {
-            if (player.isPlaying()) {
-                player.stop();
-            }
-            player.release();
+        if (binding.fullScreenPlay.isPlaying()){
+            binding.fullScreenPlay.stopPlayback();
         }
     }
 }
